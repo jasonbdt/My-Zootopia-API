3a63e4958ff931b6bd385e862d09c06dca445a13
jasonbdt <ja.bladt@gmail.com>
2025-09-30 13:45:39 +0200

    Configure dotenv to retrieve sensitive data from .env file

--- animals_api/main.py
+++ animals_api/main.py
@@ -1,8 +1,13 @@
 from typing import Any
+import os
+
+from dotenv import load_dotenv
 import requests
 
+load_dotenv()
+
 API_BASE = "https://api.api-ninjas.com/v1/animals"
-API_KEY = "qoWNFWqBIRsslGC4k0EoDA==DuuApyPyQlGpXs8J"
+API_KEY = os.getenv("API_KEY")
 
 def get_animal(name: str) -> dict[str, Any]:
     return requests.get(API_BASE, {
